7ff6fa56ab0838d6c3af43acbb5a58c4bf87713f
Matheus Ribeiro Alves <matheusr21a@gmail.com>
2026-05-27 19:47:16 -0300

    feat:improvement

--- ProjectStudy/login_system.py
+++ ProjectStudy/login_system.py
@@ -1,22 +1,38 @@
+
+
+
+
+tentativa_login = 0
+
+
 while True:
 
     print('--------menu--------')
 
-    print('1 - Login')
-    print('2 - Cadastrar')
+    print('1 - Cadastrar')
+    print('2 - Login')
     print('3 - Sair') 
+    
 
     opcao = int(input('Escolha uma opção: '))
     if opcao == 1:
-        print('Digite seu email e senha.')
-        email = input('Email: ')
-        senha = input('Senha: ')
-        print('Login realizado com sucesso!')
-    elif opcao == 2:
         print('Digite seu email e senha para cadastrar.')
+        email_cadastro = input('Email: ')
+        senha_cadastro = input('Senha: ')
+        print('Cadastro realizado com sucesso!')
+        
+    elif opcao == 2:
+        print('Digite seu email e senha para login.')
         email = input('Email: ')
         senha = input('Senha: ')
-        print('Cadastro realizado com sucesso!')
+        if email == email_cadastro and senha == senha_cadastro:
+            print('Login realizado com sucesso!')
+        else:
+            print('Email ou senha incorretos.')
+            tentativa_login += 1
+            if tentativa_login == 3:
+                print('Número de tentativas excedido. Saindo do sistema...')
+                break
         
     elif opcao == 3:
         print('Saindo do sistema...')
